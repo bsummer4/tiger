871dacdd14e8449028831d0d02686d3c1af8757e
Benjamin Summers <bsummer4@eecs.utk.edu>
2011-03-11 08:24:31 -0500

    Minor cleaning.

--- interact.sh
+++ interact.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
-while read x; do echo $x | $1; done
+#!/bin/sh -e
+
+[ 0 -ne $# ]
+prog=$1
+shift
+
+if [ 0 = $# ]
+then while read x; do echo $x | $prog; done
+else echo "$*" | $prog
+fi
