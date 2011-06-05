c55e50b86a2efa7771d16e40924217b9ee8483b7
Benjamin Summers <bsummer4@eecs.utk.edu>
2011-06-05 15:11:38 -0400

    #line annotations in m4 output.

--- m4.mk
+++ m4.mk
@@ -1,6 +1,6 @@
 # TODO Use regexp patterns
 %.sml: %.sml.m4
-	m4 <$stem.sml.m4 >$stem.sml
+	m4 -s $stem.sml.m4 | ./fixlines >$stem.sml
 tiger.lex: tiger.lex.m4
 	stem=tiger
-	m4 <$stem.lex.m4 >$stem.lex
+	m4 $stem.lex.m4 >$stem.lex
